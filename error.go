c8f6d8a73653fd932d4948a4545d7096bd15a32b
Akshay Shah <akshay@akshayshah.org>
2023-06-04 22:05:00 -0700

    Rename unexported codec to make space for binary
    To make some space for binary codec support, rename `codec` to
    `jsonCodec`. Along the way, move the error-creating utility function to
    its own file.

---
+++ error.go
@@ -0,0 +1,14 @@
+package connectproto
+
+import (
+	"fmt"
+
+	"google.golang.org/protobuf/runtime/protoiface"
+)
+
+func errNotProto(msg any) error {
+	if _, ok := msg.(protoiface.MessageV1); ok {
+		return fmt.Errorf("%T uses github.com/golang/protobuf, but connect-go only supports google.golang.org/protobuf: see https://go.dev/blog/protobuf-apiv2", msg)
+	}
+	return fmt.Errorf("%T doesn't implement proto.Message", msg)
+}
--- json.go
+++ json.go
@@ -4,33 +4,31 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"fmt"
 
 	"github.com/bufbuild/connect-go"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
-	"google.golang.org/protobuf/runtime/protoiface"
 )
 
 // WithJSON customizes a connect-go Client or Handler's JSON handling.
 func WithJSON(marshal protojson.MarshalOptions, unmarshal protojson.UnmarshalOptions) connect.Option {
 	return connect.WithOptions(
-		connect.WithCodec(&codec{name: "json", marshal: marshal, unmarshal: unmarshal}),
-		connect.WithCodec(&codec{name: "json; charset=utf-8", marshal: marshal, unmarshal: unmarshal}),
+		connect.WithCodec(&jsonCodec{name: "json", marshal: marshal, unmarshal: unmarshal}),
+		connect.WithCodec(&jsonCodec{name: "json; charset=utf-8", marshal: marshal, unmarshal: unmarshal}),
 	)
 }
 
-type codec struct {
+type jsonCodec struct {
 	name      string
 	marshal   protojson.MarshalOptions
 	unmarshal protojson.UnmarshalOptions
 }
 
-func (c *codec) Name() string { return c.name }
+func (j *jsonCodec) Name() string { return j.name }
 
-func (c *codec) IsBinary() bool { return false }
+func (j *jsonCodec) IsBinary() bool { return false }
 
-func (c *codec) Unmarshal(binary []byte, msg any) error {
+func (j *jsonCodec) Unmarshal(binary []byte, msg any) error {
 	pm, ok := msg.(proto.Message)
 	if !ok {
 		return errNotProto(msg)
@@ -38,23 +36,23 @@ func (c *codec) Unmarshal(binary []byte, msg any) error {
 	if len(binary) == 0 {
 		return errors.New("zero-length payload is not a valid JSON object")
 	}
-	return c.unmarshal.Unmarshal(binary, pm)
+	return j.unmarshal.Unmarshal(binary, pm)
 }
 
-func (c *codec) Marshal(msg any) ([]byte, error) {
+func (j *jsonCodec) Marshal(msg any) ([]byte, error) {
 	pm, ok := msg.(proto.Message)
 	if !ok {
 		return nil, errNotProto(msg)
 	}
-	return c.marshal.Marshal(pm)
+	return j.marshal.Marshal(pm)
 }
 
-func (c *codec) MarshalStable(message any) ([]byte, error) {
+func (j *jsonCodec) MarshalStable(message any) ([]byte, error) {
 	// protojson doesn't offer deterministic output. It does order fields by
 	// number, but it deliberately introduce inconsistent whitespace (see
 	// https://github.com/golang/protobuf/issues/1373). To make the output as
 	// consistent as possible, we'll need to normalize.
-	uncompacted, err := c.Marshal(message)
+	uncompacted, err := j.Marshal(message)
 	if err != nil {
 		return nil, err
 	}
@@ -63,11 +61,4 @@ func (c *codec) MarshalStable(message any) ([]byte, error) {
 		return nil, err
 	}
 	return compacted.Bytes(), nil
-}
-
-func errNotProto(msg any) error {
-	if _, ok := msg.(protoiface.MessageV1); ok {
-		return fmt.Errorf("%T uses github.com/golang/protobuf, but connect-go only supports google.golang.org/protobuf: see https://go.dev/blog/protobuf-apiv2", msg)
-	}
-	return fmt.Errorf("%T doesn't implement proto.Message", msg)
 }
